370ecb4b0a85545cff961b0130fd17ba99892e04
icsd16146 <62210290+icsd16146@users.noreply.github.com>
2023-03-10 22:08:33 +0200

    Delete DeleteR.m

--- DeleteR.m
+++
@@ -1,7 +0,0 @@
-function delR = DeleteR (apoEpo,dkmo,kkmo)
-  delR=zeros(59,1);
-  delR(1)=((apoEpo(1)+apoEpo(2))/2) + ((kkmo(2)+kkmo(3))/2);
-  for(i=2:59)
-    delR(i,1)=apoEpo(i,1)/dkmo(1,i);
-  end
-endfunction
