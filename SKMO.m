ac69af6dfb7208a56495e5bdfca3cbf52ee1f0a9
icsd16146 <62210290+icsd16146@users.noreply.github.com>
2023-03-10 22:11:55 +0200

    Delete SKMO.m

--- SKMO.m
+++
@@ -1,9 +0,0 @@
-function skmo = SKMO (Y,m)
-  m = mod(length(Y),2); %=1
-  skmo=zeros(length(Y),1);
-  a=[0.1, 0.2,  0.4,  0.2,  0.1];
-  for(t=1+m:length(Y)-m)
-    Yt=Y(t-m:t+m);
-    skmo(t,1)= a(2)*Yt(1) + a(3)*Yt(2) + a(4)*Yt(3);
-  end
-endfunction
